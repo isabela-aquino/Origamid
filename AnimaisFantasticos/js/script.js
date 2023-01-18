07a1496c2e23ec4483233270a0cc4f2af9b6e32d
Isabela Aquino <isabelaaquinom@outlook.com>
2023-01-17 21:28:21 -0300

    construindo tooltip

--- AnimaisFantasticos/js/script.js
+++ AnimaisFantasticos/js/script.js
@@ -3,12 +3,13 @@ import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
+import iniToolTip from './modules/tooltip.js'
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
 initModal();
-
+iniToolTip();
 
 
