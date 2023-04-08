bd0e7a4cff0a819519e8069a2f7dda73abfc17a4
Isabela Aquino <isabelaaquinom@outlook.com>
2023-04-07 21:01:38 -0300

    perguntas e galeria

--- bikcraft/js/script.js
+++ bikcraft/js/script.js
@@ -1,28 +1,60 @@
 //ativar links do menu
 
 const links = document.querySelectorAll('.header-menu a')
-
-function ativarLink(link){
+links.forEach((link) =>{
   const url = location.href;
   const href = link.href;
 
   if (url.includes(href)){
     link.classList.add("ativo")
   }
-}
-
-links.forEach(ativarLink)
+})
 
 // ativar itens do orçamento
 
 const parametros = new URLSearchParams(location.search)
 
-function ativarProduto(parametro){
+parametros.forEach((parametro) =>{
   const elemento = document.getElementById(parametro)
   if(elemento){
     elemento.checked = true
   }
+})
+
+//perguntas frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button');
+const ativaClass = 'ativa';
+
+function activeAccordion(){
+  const pergunta = event.currentTarget
+  const controls = pergunta.getAttribute("aria-controls")
+  const resposta = document.getElementById(controls)
+  
+  resposta.classList.toggle(ativaClass)
+  const ativo = resposta.classList.contains(ativaClass)
+  pergunta.setAttribute("aria-expanded", ativo)
+}   
+
+perguntas.forEach((item) =>{
+  item.addEventListener('click', activeAccordion);
+})
+
+//Galeria de Bicicleta
+
+const galeria = document.querySelectorAll('.bicicleta-imagens img')
+const galeriaContainer = document.querySelector('.bicicleta-imagens')
+
+function trocarImagem(event){
+  const img = event.currentTarget
+  const midia = matchMedia('(min-width: 1000px)').matches
+  const midia1 = matchMedia('(min-width: 800px)').matches
+  if(midia || !midia1){
+    galeriaContainer.prepend(img)
+  }
+  
 }
 
-parametros.forEach(ativarProduto)
-
+galeria.forEach((img) => {
+  img.addEventListener("click", trocarImagem)
+})
